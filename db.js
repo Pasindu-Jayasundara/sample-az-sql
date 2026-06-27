43548ae2e41732cba3bf5bf822c1bb03abd7943d
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2026-06-27 08:42:48 +0530

    connection config

--- db.js
+++ db.js
@@ -1,19 +1,6 @@
 import { Connection } from "tedious";
 
-const config = {
-    server: "asm02.database.windows.net",
-    authentication: {
-        type: "default",
-        options: {
-            userName: "pasindu328@",
-            password: "Bhathiya28@"
-        }
-    },
-    options: {
-        encrypt: true,
-        database: "free-sql-db-8739882"
-    }
-};
+// const config = 
 
 const con = new Connection(config);
 
